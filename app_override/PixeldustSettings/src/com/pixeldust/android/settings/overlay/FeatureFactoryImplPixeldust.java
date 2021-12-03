37643c4ca6ef310dade9b319f79a8b36b3b8a810
Anay Wadhera <awadhera@berkeley.edu>
2021-12-02 19:51:21 -0500

    PixeldustSettings: Import PowerUsageFeatureGoogle
    sourajitk: Implement to PixeldustSettings.
    
    Change-Id: I50ac0fd993eaba0e8496e7a86501b2da687e0496
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/6376
    Reviewed-by: Sourajit Karmakar <sourajit@live.com>
    Tested-by: Sourajit Karmakar <sourajit@live.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- app_override/PixeldustSettings/src/com/pixeldust/android/settings/overlay/FeatureFactoryImplPixeldust.java
+++ app_override/PixeldustSettings/src/com/pixeldust/android/settings/overlay/FeatureFactoryImplPixeldust.java
@@ -1,6 +1,21 @@
 package com.pixeldust.android.settings.overlay;
 
+import android.content.Context;
+
 import com.android.settings.overlay.FeatureFactoryImpl;
+import com.android.settings.fuelgauge.PowerUsageFeatureProvider;
+import com.google.android.settings.fuelgauge.PowerUsageFeatureProviderGoogleImpl;
 
 public final class FeatureFactoryImplPixeldust extends FeatureFactoryImpl {
+
+    private PowerUsageFeatureProvider mPowerUsageFeatureProvider;
+
+    @Override
+    public PowerUsageFeatureProvider getPowerUsageFeatureProvider(Context context) {
+        if (mPowerUsageFeatureProvider == null) {
+            mPowerUsageFeatureProvider = new PowerUsageFeatureProviderGoogleImpl(
+                    context.getApplicationContext());
+        }
+        return mPowerUsageFeatureProvider;
+    }
 }
