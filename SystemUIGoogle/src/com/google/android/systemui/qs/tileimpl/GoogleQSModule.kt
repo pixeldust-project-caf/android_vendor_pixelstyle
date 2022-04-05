8b600c1fe36a60767e44c7e88bc9626ab4e86095
spezi77 <spezi7713@gmx.net>
2022-04-05 19:14:23 +0200

    SystemUIGoogle: Squashed import of our qs tiles. Full commit history/authorship can be found here: https://github.com/pixeldust-project-caf/android_frameworks_base/commits/saltedcaramel/packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java Updated for A14 following: * https://github.com/PixelDust-next/android_frameworks_base/commit/346a9f618d5413152cb53dd3a61e4262f6b22178 * https://github.com/PixelDust-next/android_frameworks_base/commit/09070e3117652240508df727392ad796d1d58140
    Squashed-in:
    * SystemUIGoogle: Use secondary label for screenshot QS tile
    * SystemUIGoogle: Use secondary label for language QS tile
    * SystemUIGoogle: Change default value in AODTile to match fw/b.
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: I5f9642a9b25e07699984c11dc725a1ecb9c54da2

--- SystemUIGoogle/src/com/google/android/systemui/qs/tileimpl/GoogleQSModule.kt
+++ SystemUIGoogle/src/com/google/android/systemui/qs/tileimpl/GoogleQSModule.kt
@@ -19,6 +19,16 @@ package com.google.android.systemui.qs.tileimpl;
 import com.android.systemui.qs.tileimpl.QSTileImpl
 import com.google.android.systemui.qs.tiles.BatterySaverTileGoogle;
 import com.google.android.systemui.qs.tiles.ReverseChargingTile;
+
+// Pixeldust qs tiles
+import com.pixeldust.android.systemui.qs.tiles.AODTile
+import com.pixeldust.android.systemui.qs.tiles.CaffeineTile
+import com.pixeldust.android.systemui.qs.tiles.DataSwitchTile
+import com.pixeldust.android.systemui.qs.tiles.LocaleTile
+import com.pixeldust.android.systemui.qs.tiles.ScreenshotTile
+import com.pixeldust.android.systemui.qs.tiles.SyncTile
+import com.pixeldust.android.systemui.qs.tiles.VpnTile
+
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
@@ -38,4 +48,46 @@ interface GoogleQSModule {
     @IntoMap
     @StringKey(BatterySaverTileGoogle.TILE_SPEC)
     fun bindBatterySaverTileGoogle(batterySaverTileGoogle: BatterySaverTileGoogle): QSTileImpl<*>
+
+    /** Inject AODTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(AODTile.TILE_SPEC)
+    fun bindAODTile(aodTile: AODTile): QSTileImpl<*>
+
+    /** Inject CaffeineTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(CaffeineTile.TILE_SPEC)
+    fun bindCaffeineTile(caffeineTile: CaffeineTile): QSTileImpl<*>
+
+    /** Inject DataSwitchTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(DataSwitchTile.TILE_SPEC)
+    fun bindDataSwitchTile(dataSwitchTile: DataSwitchTile): QSTileImpl<*>
+
+    /** Inject LocaleTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(LocaleTile.TILE_SPEC)
+    fun bindLocaleTile(localeTile: LocaleTile): QSTileImpl<*>
+
+    /** Inject ScreenshotTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(ScreenshotTile.TILE_SPEC)
+    fun bindScreenshotTile(screenshotTile: ScreenshotTile): QSTileImpl<*>
+
+    /** Inject SyncTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(SyncTile.TILE_SPEC)
+    fun bindSyncTile(syncTile: SyncTile): QSTileImpl<*>
+
+    /** Inject VpnTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(VpnTile.TILE_SPEC)
+    fun bindVpnTile(vpnTile: VpnTile): QSTileImpl<*>
 }
